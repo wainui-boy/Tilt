a946fc6c015e8f342f6c0a540181005641582b1d
wainui-boy <49776612+wainui-boy@users.noreply.github.com>
2023-04-01 15:32:41 +1100

    Update tilt_read.py

--- tilt_read.py
+++ tilt_read.py
@@ -3,18 +3,18 @@
 import subprocess
 
 # Set the MAC address of your Tilt hydrometer
-tilt_mac_address = "XX:XX:XX:XX:XX:XX"
+tilt_mac_address = "04:3E:2A:02:01:03:01"
 
 # Turn on Bluetooth
-subprocess.call("sudo hciconfig hci0 up > /dev/null 2>&1 &", shell=True)
+subprocess.call("sudo hciconfig hci0 up", shell=True)
 
-# Start scanning for nearby Bluetooth LE devices in the background
-subprocess.Popen("sudo hcitool lescan > /dev/null 2>&1 &", shell=True)
+# Scan for nearby Bluetooth LE devices
+subprocess.call("sudo hcitool lescan", shell=True)
 
 # Listen for broadcasts from the Tilt hydrometer
 subprocess.call(f"sudo hcitool lecc {tilt_mac_address}", shell=True)
 subprocess.call(f"sudo hcitool ledc {tilt_mac_address} 0x0000", shell=True)
-tilt_output = subprocess.Popen("sudo hcidump --raw", stdout=subprocess.PIPE)
+tilt_output = subprocess.Popen("sudo hcidump --raw", stdout=subprocess.PIPE, shell=True)
 
 # Parse the Tilt hydrometer's data from the broadcast output
 for line in iter(tilt_output.stdout.readline, b''):
@@ -29,4 +29,3 @@ for line in iter(tilt_output.stdout.readline, b''):
             
             # Do something with the data (e.g., print it to the console)
             print(f"Color: {color}, Temperature: {temperature}, Gravity: {gravity}")
-
