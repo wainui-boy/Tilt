8d2bdca881c9fccf9c4ff08037519442f631175e
wainui-boy <49776612+wainui-boy@users.noreply.github.com>
2023-04-01 17:14:55 +1100

    Update tilt_read.py

--- tilt_read.py
+++ tilt_read.py
@@ -1,22 +1,22 @@
-#!/usr/bin/env python3
-
 from bluepy.btle import Scanner, DefaultDelegate
 import argparse
 
 
 class ScanDelegate(DefaultDelegate):
-    def __init__(self):
+    def __init__(self, mac):
         DefaultDelegate.__init__(self)
+        self.mac = mac
+        self.found = False
 
     def handleDiscovery(self, dev, isNewDev, isNewData):
-        if dev.addr == args.mac and isNewData:
+        if dev.addr == self.mac and isNewData:
+            self.found = True
             for (adtype, desc, value) in dev.getScanData():
                 if desc == 'Manufacturer':
                     temp = int(value[6:10], 16) / 100
                     gravity = int(value[10:14], 16) / 1000
                     print("Temperature: %.2f C" % temp)
                     print("Gravity: %.3f" % gravity)
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -25,5 +25,8 @@ if __name__ == "__main__":
                         help="scan timeout in seconds (default: 10)")
     args = parser.parse_args()
 
-    scanner = Scanner().withDelegate(ScanDelegate())
+    scanner = Scanner().withDelegate(ScanDelegate(args.mac))
     devices = scanner.scan(args.timeout)
+
+    if not scanner.delegate.found:
+        print("Could not find Tilt Hydrometer with MAC address %s" % args.mac)
